c2c2b5d1c96be09972d8b2b13621da8b5bc52cc1
Tsz Him Ko <93413617+tommy9756834@users.noreply.github.com>
2025-07-26 13:10:24 -0400

    added command

--- autobackup.py
+++ autobackup.py
@@ -4,49 +4,48 @@ import subprocess
 import sys
 import os
 import datetime
-#sources https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
 import glob
 import re
 
 def logging(message):
-     logpath = re.sub(r'\/[^/]*$',"/log.txt",sys.argv[0])
-     time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+     logpath = re.sub(r'\/[^/]*$',"/log.txt",sys.argv[0]) #replace the end of current path to /log.txt
+     time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") #time stamp
      try:
-          file = open(logpath,"a")
-          file.write(f"[{time}] {message}\n")
+          file = open(logpath,"a") #open log file for logging
+          file.write(f"[{time}] {message}\n") #write into the log file
           file.close()
      except:
           pass
 
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-target_path = sys.argv[1]
-dest_path = f"{sys.argv[2]}"
-backup_name = f"{sys.argv[3]}_{timestamp}"
+target_path = sys.argv[1] #target path from cron job for example: "-C /home/thko1/ops445/a2 things"
+dest_path = f"{sys.argv[2]}" #destination path for example: "/home/thko1/ops445/backup/"
+backup_name = f"{sys.argv[3]}_{timestamp}" 
 
 try:
-    os.makedirs(dest_path, exist_ok=True)
+    os.makedirs(dest_path, exist_ok=True) #check if it can make file in the destination path 
 except IOError:
-     logging("Error: Auto backup failed. Deleteing crontab...")
-     removecroncmd = f'crontab -l | grep -v \'{sys.argv[0]} "{sys.argv[1]}" {sys.argv[2]} {sys.argv[3]}\' | crontab -'
-     process = subprocess.Popen(removecroncmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)   
+     logging("Error: Auto backup failed. Deleteing crontab...") #make log of backup failed
+     removecroncmd = f'crontab -l | grep -v \'{sys.argv[0]} "{sys.argv[1]}" {sys.argv[2]} {sys.argv[3]}\' | crontab -' #delete cronjob command
+     process = subprocess.Popen(removecroncmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) #run the delete cronjob command
      exit()
 
-tar_cmd = f"tar cvzf {dest_path}{backup_name}.tar.gz {target_path}"
-process = subprocess.Popen(tar_cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-process.wait()
+tar_cmd = f"tar cvzf {dest_path}{backup_name}.tar.gz {target_path}" #tar command for backup
+process = subprocess.Popen(tar_cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) #run the tar command
+process.wait() # wait a bit
 
-if process.returncode == 0:
+if process.returncode == 0: #if backup is sucessful
      target = sys.argv[1].rsplit(" ",2)
      target = target[1] + "/" + target[2]
-     logging(f"Auto backup of {target} was sucessful!")
-else:
-     logging("Error: Auto backup failed. Deleteing crontab...")
-     removecroncmd = f'crontab -l | grep -v \'{sys.argv[0]} "{sys.argv[1]}" {sys.argv[2]} {sys.argv[3]}\' | crontab -'
-     process = subprocess.Popen(removecroncmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+     logging(f"Auto backup of {target} was sucessful!") #log a message of backup sucessful
+else: #if backup is not sucessful
+     logging("Error: Auto backup failed. Deleteing crontab...") #log a message of backup not sucessful
+     removecroncmd = f'crontab -l | grep -v \'{sys.argv[0]} "{sys.argv[1]}" {sys.argv[2]} {sys.argv[3]}\' | crontab -' #delete cronjob command
+     process = subprocess.Popen(removecroncmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) #run deletecron job command
      exit()
 
-filelist = glob.glob(f"{dest_path}{sys.argv[3]}_*_*.tar.gz")
-if len(filelist) > 10:
-     filelist.sort()
-     os.remove(filelist[0])
-     logging(f"Older backup of {filelist[0]} was removed!")
+filelist = glob.glob(f"{dest_path}{sys.argv[3]}_*_*.tar.gz") #get all the backup file path in a list
+if len(filelist) > 10: # if there are more than 10 backup
+     filelist.sort() #sort the list from old to new
+     os.remove(filelist[0]) #delete the oldest backup
+     logging(f"Older backup of {filelist[0]} was removed!") #log the backup deleted
