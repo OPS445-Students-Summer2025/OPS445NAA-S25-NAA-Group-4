cfda7cf54cbdf125f369e408b391a363cdb2270b
Tsz Him Ko <thko1@myseneca.ca>
2025-07-26 00:15:46 -0400

    added logging

--- autobackup.py
+++ autobackup.py
@@ -6,6 +6,17 @@ import os
 import datetime
 #sources https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
 import glob
+import re
+
+def logging(message):
+     logpath = re.sub(r'\/[^/]*$',"/log.txt",sys.argv[0])
+     time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+     try:
+          file = open(logpath,"a")
+          file.write(f"[{time}] {message}\n")
+          file.close()
+     except:
+          pass
 
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 target_path = sys.argv[1]
@@ -15,18 +26,27 @@ backup_name = f"{sys.argv[3]}_{timestamp}"
 try:
     os.makedirs(dest_path, exist_ok=True)
 except IOError:
-        exit()
+     logging("Error: Auto backup failed. Deleteing crontab...")
+     removecroncmd = f'crontab -l | grep -v \'{sys.argv[0]} "{sys.argv[1]}" {sys.argv[2]} {sys.argv[3]}\' | crontab -'
+     process = subprocess.Popen(removecroncmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)   
+     exit()
+
 tar_cmd = f"tar cvzf {dest_path}{backup_name}.tar.gz {target_path}"
 process = subprocess.Popen(tar_cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 process.wait()
 
 if process.returncode == 0:
-     pass
+     target = sys.argv[1].rsplit(" ",2)
+     target = target[1] + "/" + target[2]
+     logging(f"Auto backup of {target} was sucessful!")
 else:
+     logging("Error: Auto backup failed. Deleteing crontab...")
      removecroncmd = f'crontab -l | grep -v \'{sys.argv[0]} "{sys.argv[1]}" {sys.argv[2]} {sys.argv[3]}\' | crontab -'
      process = subprocess.Popen(removecroncmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+     exit()
 
 filelist = glob.glob(f"{dest_path}{sys.argv[3]}_*_*.tar.gz")
 if len(filelist) > 10:
      filelist.sort()
      os.remove(filelist[0])
+     logging(f"Older backup of {filelist[0]} was removed!")
