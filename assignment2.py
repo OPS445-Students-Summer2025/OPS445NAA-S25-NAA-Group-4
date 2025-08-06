de63529c59f133a0aa0d12ae29e5cdb0b96380e4
Charles Shin <cshin9@myseneca.ca>
2025-08-06 14:39:45 -0400

    Added UI and readme desc to assignment2.py and revamped restore.py to accept cmd arguments

--- assignment2.py
+++ assignment2.py
@@ -3,41 +3,78 @@
 '''
 OPS445 Assignment 2
 Program: assignment2.py 
-The python code in this file is original work written by
-"OPS445 NAA GROUP 4". No code in this file is copied from any other source
-except those provided by the course instructor, including any person,
-textbook, or on-line resource. We have not shared this python script
-with anyone or anything except for submission for grading. We understand
-that the Academic Honesty Policy will be enforced and
+The python code in this file is original work written by 
+"OPS445 NAA GROUP 4". No code in this file is copied and pasted 
+from any other source except those provided by on-line resources 
+where sources are documented. We have not shared this python script
+with anyone or anything except for submission for grading. We 
+understand that the Academic Honesty Policy will be enforced and
 violators will be reported and appropriate action will be taken.
 
-Author: <OPS445 NAA GROUP 4>
+Authors: <OPS445 NAA GROUP 4 Group Members:
+          Charles Shin
+          Kang Kang
+          Tsz Him Ko>
 Semester: <Summer> <2025>
-Description: <Script will perform backup and restore capabilities
-              sometime in the near future.>
+Description: <Script will perform backup and restore capabilities.
+              Backup can be done manually through the commandline
+              interface or through a user guided process. Autobackup
+              can also be set up as well to regularly backup a certain
+              file/dir using crontab as the scheduling program. This
+              program assumes the user is using a Linux based OS with
+              the following modules/python scripts downloaded and in
+              the same directory:
+              
+              assignment2.py
+              autobackup.py
+              backup.py
+              restore.py
+              
+              This script and it's import modules also utilize
+              python libraries and Linux tools listed below:
+              
+              os
+              sys
+              subprocess
+              datetime
+              re
+              glob
+              argparse
+              tarfile
+              
+              cron
+              rsync
+              
+              This program assumes that if the user did not backup
+              files using this program that when running the restore
+              program they have compressed their files using tar.>
 '''
 import backup
+import restore
 import os
 
 #Run Main Code here as the menu
 if __name__ == '__main__':
 
-    #Menu Dialog to user to gain their input/choice
+    #Menu Dialog to user to gain their input/choice to see if they would like to backup or restore
     choice = input("Welcome to the Backup and Restore Program!\nWhat would you like to do?\nPlease type in your choice according to the number and press ENTER\n1. Backup\n2. Restore\n3. Exit\n")
     
     if choice == '1':
-        #Do Backup code and prompts here
-        #Right now, still trying to figure out how to pass in cmd line arguments from this program to backup.main()
+        #The backup.py main function is run. It works alongside the autobackup.py module
         os.system("clear")
         backup.main()
+        exit()
     
     if choice == '2':
         #do Restore code
-        pass
-
-    if choice == '3':
+        os.system("clear")
+        restore.main()
         exit()
 
+    if choice == '3':
+        print("Exiting...")
+        exit()
     
+    print("An incorrect option was inputted, Exiting...")
     exit()
     
--- restore.py
+++ restore.py
@@ -1,50 +1,63 @@
 #!/usr/bin/env python3
 
-import subprocess
-import sys
 import os
-import datetime
-import re
 import tarfile
+import argparse
+import sys
 
-#note will need to add autoback up stuff and also account for uncompressed/compressed files
+#note will need to account for uncompressed/compressed files
 #add a restore log for later
+#fix up the wacky argparse stuff
+#allow the user to pick which files/dirs they would like to restore
+#checker to see if the area where the restore program is trying to restore to has enough space?
 
 def main():
     '''
     Code will run the main Restore Module below, this restore program assumes that the
-    user zipped or compressed their backup files beforehand to save space in archiving
+    user zipped or compressed their backup files beforehand to save space in archiving.
+    The user can also run this program from the command line with arguments to
+    take advantage of automation.
     '''
 
-    #Try statement will attempt to run the Restore program and check and break off
-    #with reasonable error statement to user to explain their mistake
-    try:
+    #Clean up the screen
+    os.system("clear")
 
-        os.system("clear")
-        choice = input("Welcome to the Restore Program! What would you like to do?\n1. Manually Restore\n2. Setup Auto Restore\n")
+    #Create argparse parser to extract source and destination filepaths from user if 
+    #they choose to run program from commandline to make it faster and for automation
+    restore_parser = argparse.ArgumentParser()
+
+    restore_parser.add_argument('--source_path', type=str, help="filepath source of your backup")
+    restore_parser.add_argument('--dest_path', type=str, help="filepath destination to restore backup")
+    
+    args = restore_parser.parse_args()
+
+    #Only run the commandline arguments to restore if the acceptable amount of arguments
+    #are met to prevent crashing. This includes the flags and the filename.
+    if len(sys.argv) == 5:
+        restore(args.source_path, args.dest_path)
+
+    #Run the program with a user friendly restore experience in the case the proper
+    #arugements are not met.
+    else:
+        #prob don't need the help...
+        print("Welcome to the Restore Program!\nIf you would like to run this program from the Command Line, "
+             f"the usage is as follows:\n{restore_parser.prog} --source_path source_filepath --dest_path destination_filepath")
+        print("Optionally, you may also continue to follow this program to guide you through the process.")
+
+        source_path = input("Please type in the filepath of the file/dir backup you would like to restore:")
+        dest_path = input("Please type in the filepath destination where you would like to restore your backup:")
         
-        #Redirects user choice to functions that will run users wanted option
-        if choice == '1':
-            manual_restore()
-        if choice == '2':
-            pass
-
-    except:
-        print("An incorrect option was entered. Please rerun the program and try again")
+        restore(source_path, dest_path)
 
 
-def manual_restore():
+def restore(source_path, dest_path):
     '''
-    Code will run manual restore program, will use either copy or rsync to do so by 
+    Code will run user guided restore program, will use tarfile module to do so by 
     retrieving user wanted source backup file/dir and unzipping it to a new directory
     '''
     try:
-        os.system("clear")
-        
-        source_path = input("You have chosen Manual Restore Mode. Please type in the filepath of the file/dir backup you would like to restore:")
-        dest_path = input("Please type in the filepath where you would like to send your backup:")
 
-        #Extract the file/dir assuming it is zipped using tarfile module, will have to make a program later that accounts for both
+        #Extract the file assuming it is zipped using tar, will have to make a program later that accounts for regular files/dirs
         #https://docs.python.org/3/library/tarfile.html#command-line-options << Source for the usage of tarfile        
         with tarfile.open(source_path, 'r') as tar:
             tar.extractall(path=dest_path)
@@ -55,6 +68,8 @@ def manual_restore():
 
     except IOError:
         print("The inputted paths are not valid, please check permissions of files/dirs and if the filepath exists and rerun the program")
+    except ValueError:
+        print("You did not input any values, please rerun the program and try again")
 
 if __name__ == "__main__":
     main()
